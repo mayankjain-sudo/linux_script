e195c0e2b05f9713ede2d6cc76b03df39764b985
Mayank <immanku7@gmail.com>
2019-09-20 12:47:45 +0530

    Check Ram utilization

--- ramalert.sh
+++ ramalert.sh
@@ -1,24 +1,11 @@
-#!/bin/bash
-
-threshold=90
-threshold2=95
-
-freemem=$(($(free -m |awk 'NR==2 {print $3}') * 100))
-
-usage=$(($freemem / 8192))
-
-if [ "$usage" -gt "$threshold" ]
-
-then
-
-/etc/init.d/apache2 restart
-
-     if [ "$usage" -gt "$threshold2" ]
-
-     then
-
-     echo "The memory usage has reached $usage% on $HOSTNAME." | mail -s "High Memory Usage Alert" abc@xyz.com
-
-
-     fi
+#!/bin/sh
+threshold1=50
+FREE=$(($(free -m | awk 'NR==2 {print $4}') * 100 ))
+echo $FREE
+USAGE=$(($FREE / 4314 ))
+echo $USAGE
+if [ $USAGE -ge $threshold1 ] ; then
+       echo "Warnig: Ram utilization reach threshold"
+else 
+       echo "Ram is under threshold limt"
 fi
